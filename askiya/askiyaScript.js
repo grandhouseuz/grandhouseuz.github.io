853678599f9e2a20a27af4a3e1ebf5cda30cd448
grandhouseuz <grandhouseuz.github.com>
2018-08-13 18:27:38 +0500

    .

--- askiya/askiyaScript.js
+++ askiya/askiyaScript.js
@@ -1,5 +1,29 @@
-window.addEventListener("load", function() {
+window.addEventListener("DOMContentLoaded", function() {
 
+    function countNumber(elemId, goal,speed) {
+        var num = 0;
+        function count(element) {
+            var elem = document.getElementById(elemId);
+            var counter = setTimeout(function() {
+                count(elemId);
+            },1);
+
+            num += speed;
+
+            if(num >= goal) {
+                clearTimeout(counter);
+                num = goal;
+                elem.textContent = num;
+                return;
+            }
+
+            elem.textContent = num;
+
+        }
+        count(elemId);
+    }
+
+    countNumber("number1",1001,5);
 
     function menuBtn() {
     var btn = document.querySelector(".menuToggleBtn");
